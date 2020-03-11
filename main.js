e5804258617cdb7ae1c64edf8de24c8ca0b50f4f
Sam Machin <sam@sammachin.com>
2020-03-11 17:43:20 +0000

    integrated ngrok

--- main.js
+++ main.js
@@ -14,11 +14,15 @@ const os = require('os');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const {Menu, MenuItem} = electron;
+const {Menu, MenuItem, clipboard, Notification} = electron;
 const Store = require('electron-store');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
+const ngrok = require('ngrok');
 
+
+
+const isMac = process.platform === 'darwin'
 
 // this should be placed at top of main.js to handle squirrel setup events quickly
 if (handleSquirrelEvent()) { return; }
@@ -26,6 +30,7 @@ if (handleSquirrelEvent()) { return; }
 var http = require('http');
 var express = require("express");
 var RED = require("node-red");
+
 // setup settings store
 const store = new Store();
 
@@ -57,15 +62,15 @@ var settings = {
     adminAuth : {
         type: "credentials",
         users: [{
-            username: store.get('nodered.username', "admin"), 
-            password: store.get('nodered.password', bcrypt.hashSync("password", 8)),
+            username: store.get('nodered-username', "admin"), 
+            password: store.get('nodered-password', bcrypt.hashSync("password", 8)),
             permissions: "*"
         }]
 }
 };
 
 // tcp port to use
-const listenPort = store.get('nodered.listenPort') || "1880"; 
+const listenPort = store.get('nodered-port') || "1880"; 
 
 // Initialise the runtime with a server and settings
 RED.init(server,settings);
@@ -76,48 +81,105 @@ red_app.use(settings.httpAdminRoot,RED.httpAdmin);
 // Serve the http nodes UI from /api
 red_app.use(settings.httpNodeRoot,RED.httpNode);
 
-function noderedSettings() {
-    const nrsettingsWindow = new BrowserWindow({
+function openSettings() {
+    const settingsWindow = new BrowserWindow({
       webPreferences: {
             nodeIntegration: true
       },
       width: 600,
-      height: 400,
+      height: 800,
     })
-    nrsettingsWindow.once('ready-to-show', () => {
-        nrsettingsWindow.show()
+    settingsWindow.once('ready-to-show', () => {
+        settingsWindow.show()
     })
-    nrsettingsWindow.loadFile('nodered-settings.html')
+    settingsWindow.loadFile('settings.html')
+  }
+
+  let ngrokConnected = false;
+
+  function toggleNgrok() {
+    if (!ngrokConnected){
+      (async function() {
+        const url = await ngrok.connect();
+        ngrokConnected = true
+        ngrokToast(url, ngrokConnected)
+        template[4].submenu[0].label = url
+        template[4].submenu[0].click = function(){clipboard.writeText(url, 'selection')}
+        template[4].submenu[0].enabled = true
+        template[4].submenu[4].enabled = true
+        template[4].submenu[2].label = "Disconnect"
+        const menu = Menu.buildFromTemplate(template)
+        Menu.setApplicationMenu(menu)
+      })();
+    }
+    (async function() {
+      const url = await ngrok.disconnect();
+      ngrokConnected = false
+      ngrokToast(null, ngrokConnected)
+      template[4].submenu[0].label = "Not Connected"
+      template[4].submenu[0].enabled = false
+      template[4].submenu[4].enabled = false
+      template[4].submenu[2].label = "Connect"
+      const menu = Menu.buildFromTemplate(template)
+      Menu.setApplicationMenu(menu)
+    })();
+  }
+  
+  function inspectNgrok(){
+    let ngrokWin = new BrowserWindow({ width: 1024, height: 768 })
+    ngrokWin.on('closed', () => {
+      ngrokWin = null
+    })
+    ngrokWin.loadURL('http://127.0.0.1:4040')
+  }
+  
+  
+  function ngrokToast(url, connected){
+    if (connected){
+      const options = {
+        title : 'Connected',
+        body : url
+      }
+      let notification = new Notification(options)
+      notification.show()
+    } else {
+      const options = {
+        title : 'Disconnected',
+        silent : true
+      }
+      let notification = new Notification(options)
+      notification.show()
+    }
+    
   }
   
 
+
 // Create the Application's main menu
-var template = [{
-    label: "Application",
-    submenu: [
-        { role: 'about' },
-        { type: "separator" },
-        { role: 'quit' }
-    ]}, {
-    label: 'Node-RED',
-    submenu: [
-        { label: 'Settings',
-        click: function(){
-            noderedSettings();
-          }
-        },
-        { label: 'Documentation',
-        click() { require('electron').shell.openExternal('http://nodered.org/docs') }
-        },
-        { label: 'Flows and Nodes',
-        click() { require('electron').shell.openExternal('http://flows.nodered.org') }
-        },
-        { label: 'Google group',
-        click() { require('electron').shell.openExternal('https://groups.google.com/forum/#!forum/node-red') }
-        }
-    ]}, {
-    label: "Edit",
-    submenu: [
+var template = [
+    ...(isMac ? [{
+        label: 'Workbench',
+        submenu: [
+          { role: 'about' },
+          { type: 'separator' },
+          { label: 'Settings', click: openSettings },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideothers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' }
+        ]
+      }] : []),
+      {
+        label: 'File',
+        submenu: [
+          isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+      },
+    { label: "Edit", submenu: [
         { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
         { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
         { type: "separator" },
@@ -125,9 +187,8 @@ var template = [{
         { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
         { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
         { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}, {
-    label: 'View',
-    submenu: [
+    ]}, 
+    { label: 'View', submenu: [
         { label: 'Reload',
             accelerator: 'CmdOrCtrl+R',
             click(item, focusedWindow) { if (focusedWindow) focusedWindow.reload(); }
@@ -143,7 +204,36 @@ var template = [{
         { type: 'separator' },
         { role: 'togglefullscreen' },
         { role: 'minimize' }
-    ]}
+    ]},
+    { label: 'ngrok', submenu: [
+          { label: 'Not Connected', enabled: false},
+          { type: 'separator' },
+          {
+            label: 'Connect',
+            click: function(){
+              toggleNgrok ();
+            }
+          },
+          { type: 'separator' },
+          {
+            label: 'Inspector',
+            enabled: false,
+            click: function(){
+              inspectNgrok ();
+            }
+          }
+          
+        ]
+    },
+    { label: 'Help', submenu: [
+        
+        { label: 'Node-RED Documentation',
+        click() { require('electron').shell.openExternal('http://nodered.org/docs') }
+        },
+        { label: 'Flows and Nodes',
+        click() { require('electron').shell.openExternal('http://flows.nodered.org') }
+        }
+    ]}, 
 ];
 
 let mainWindow;
---
+++ settings.js
@@ -0,0 +1,34 @@
+const Store = require('electron-store');
+const store = new Store();
+const bcrypt = require('bcryptjs');
+
+
+let form = document.getElementById("ngrok-settings")
+
+formToJson = (elements) => {
+  let json = {};
+  elements.forEach(element => {
+    if (element.name !== "") json[element.name] = element.value
+  });
+  return json;
+}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  store.set({
+    ngrok: formToJson([...form.elements])
+  })
+})
+
+function saveSettings(){
+  store.set('nodered-port', document.getElementById('nodered-port').value);
+  store.set('nodered-username', document.getElementById('nodered-username').value);
+  if (document.getElementById('nodered-password').value != null) {
+      store.set('nodered-password', bcrypt.hashSync(document.getElementById('nodered-password').value, 8))
+  }
+}
+
+function getSettings(){
+  document.getElementById('nodered-port').value = store.get('nodered-port')
+  document.getElementById('nodered-username').value = store.get('nodered-username')
+}
